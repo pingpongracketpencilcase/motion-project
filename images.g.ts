2ee7630ddae5073413679094a3f3038ad6de57db
Emily Pang <82404497+pingpongracketpencilcase@users.noreply.github.com>
2021-04-19 14:11:19 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "b0/iwuS+K/NPDz`:v_-X":
+            case "poop":return img`
+. . . . . . . e . . e . . . . . 
+. . . . . . e . . e . . . . . . 
+. . . . . . . e . . e . . . . . 
+. . . . . . e . . e . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . e e e . . . . . . 
+. . . . . . e e e e e . . . . . 
+. . . . . . e e e e e . . . . . 
+. . . . . e e e e e e e . . . . 
+. . . . . e e e e e e e . . . . 
+. . . e e e e e e e e e e e . . 
+. . . e e e e e e e e e e e . . 
+. . . e e e e e e e e e e e . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
