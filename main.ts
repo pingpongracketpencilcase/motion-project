4dede1c247fb73f86dc6b6b525dca245a84dc3da
Emily Pang <82404497+pingpongracketpencilcase@users.noreply.github.com>
2021-04-21 14:13:10 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 namespace SpriteKind {
     export const follower = SpriteKind.create()
+    export const Food2 = SpriteKind.create()
 }
 sprites.onDestroyed(SpriteKind.Food, function (sprite) {
     ice_cream = sprites.create(img`
@@ -21,6 +22,27 @@ sprites.onDestroyed(SpriteKind.Food, function (sprite) {
         . . . . . . . . . . . . . . . . 
         `, SpriteKind.Food)
 })
+sprites.onCreated(SpriteKind.Food2, function (sprite) {
+    sprite.setImage(img`
+        . . . . . 3 3 b 3 3 d d 3 3 . . 
+        . . . . 3 5 5 d 3 d 5 5 5 5 3 . 
+        . . . 3 d 5 5 5 d 5 5 5 d 3 1 3 
+        . . 3 d d 5 5 5 d d 5 5 5 3 3 3 
+        . 3 5 5 d 5 5 5 5 d d 5 5 b . . 
+        . 3 5 5 5 d 5 5 5 5 5 d 5 5 3 . 
+        . b d 5 5 5 d 5 5 5 5 5 5 5 3 . 
+        . 4 b 5 5 5 5 d d 5 5 5 5 d 3 . 
+        . 4 4 d 5 5 5 5 5 5 d d d b b . 
+        . 4 d b d 5 5 5 5 5 5 5 5 3 . . 
+        4 d d 5 b d 5 5 5 5 5 5 5 3 . . 
+        4 5 d 5 5 b b d 5 5 5 5 d 3 . . 
+        4 5 5 d 5 5 d b b b d d 3 . . . 
+        4 5 5 5 d d d d 4 4 b 3 . . . . 
+        . 4 5 5 5 4 4 4 . . . . . . . . 
+        . . 4 4 4 . . . . . . . . . . . 
+        `)
+    sprite.setPosition(randint(8, 152), randint(19, 112))
+})
 sprites.onCreated(SpriteKind.Food, function (sprite) {
     sprite.setImage(img`
         . . . . . 3 3 b 3 3 d d 3 3 . . 
@@ -40,12 +62,37 @@ sprites.onCreated(SpriteKind.Food, function (sprite) {
         . 4 5 5 5 4 4 4 . . . . . . . . 
         . . 4 4 4 . . . . . . . . . . . 
         `)
-    sprite.setPosition(randint(8, 152), randint(8, 112))
+    sprite.setPosition(randint(8, 152), randint(19, 112))
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     ice_cream.destroy()
     info.changeScoreBy(1)
 })
+sprites.onDestroyed(SpriteKind.Food2, function (sprite) {
+    ice_cream = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Food2)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food2, function (sprite, otherSprite) {
+    ice_cream_2.destroy()
+    info.changeScoreBy(1)
+})
+let ice_cream_2: Sprite = null
 let ice_cream: Sprite = null
 info.startCountdown(30)
 game.splash("The Eating Game")
@@ -88,7 +135,26 @@ ice_cream = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Food)
+ice_cream_2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Food2)
+mySprite.setBounceOnWall(true)
 game.onUpdate(function () {
-    mySprite.x += controller.dx(200)
-    mySprite.y += controller.dy(200)
+    mySprite.x += controller.dx(175)
+    mySprite.y += controller.dy(175)
 })
