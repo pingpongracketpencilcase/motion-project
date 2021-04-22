0bd6c81cbcfc3b7ddb7fc3aa96d65a3056e26074
Emily Pang <82404497+pingpongracketpencilcase@users.noreply.github.com>
2021-04-22 13:56:06 -0400

    added more foods

--- main.ts
+++ main.ts
@@ -1,9 +1,38 @@
 namespace SpriteKind {
     export const follower = SpriteKind.create()
     export const Food2 = SpriteKind.create()
+    export const Food3 = SpriteKind.create()
+    export const Food4 = SpriteKind.create()
+    export const Food5 = SpriteKind.create()
 }
+sprites.onCreated(SpriteKind.Food5, function (sprite) {
+    sprite.setImage(img`
+        . . . . . . . e c 7 . . . . . . 
+        . . . . e e e c 7 7 e e . . . . 
+        . . c e e e e c 7 e 2 2 e e . . 
+        . c e e e e e c 6 e e 2 2 2 e . 
+        . c e e e 2 e c c 2 4 5 4 2 e . 
+        c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
+        c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
+        c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+        c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+        c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+        c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
+        . e e e 2 2 2 2 2 2 2 2 2 4 e . 
+        . 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
+        . . 2 e e 2 2 2 2 2 4 4 2 e . . 
+        . . . 2 2 e e 4 4 4 2 e e . . . 
+        . . . . . 2 2 e e e e . . . . . 
+        `)
+    sprite.setPosition(randint(8, 152), randint(19, 112))
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food4, function (sprite, otherSprite) {
+    Hamburger.destroy()
+    info.changeScoreBy(1)
+})
 sprites.onDestroyed(SpriteKind.Food, function (sprite) {
-    ice_cream = sprites.create(img`
+    pause(500)
+    cherry = sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
@@ -25,27 +54,6 @@ sprites.onDestroyed(SpriteKind.Food, function (sprite) {
 sprites.onCreated(SpriteKind.Food2, function (sprite) {
     sprite.setImage(img`
         . . . . . 3 3 b 3 3 d d 3 3 . . 
-        . . . . 3 5 5 d 3 d 5 5 5 5 3 . 
-        . . . 3 d 5 5 5 d 5 5 5 d 3 1 3 
-        . . 3 d d 5 5 5 d d 5 5 5 3 3 3 
-        . 3 5 5 d 5 5 5 5 d d 5 5 b . . 
-        . 3 5 5 5 d 5 5 5 5 5 d 5 5 3 . 
-        . b d 5 5 5 d 5 5 5 5 5 5 5 3 . 
-        . 4 b 5 5 5 5 d d 5 5 5 5 d 3 . 
-        . 4 4 d 5 5 5 5 5 5 d d d b b . 
-        . 4 d b d 5 5 5 5 5 5 5 5 3 . . 
-        4 d d 5 b d 5 5 5 5 5 5 5 3 . . 
-        4 5 d 5 5 b b d 5 5 5 5 d 3 . . 
-        4 5 5 d 5 5 d b b b d d 3 . . . 
-        4 5 5 5 d d d d 4 4 b 3 . . . . 
-        . 4 5 5 5 4 4 4 . . . . . . . . 
-        . . 4 4 4 . . . . . . . . . . . 
-        `)
-    sprite.setPosition(randint(8, 152), randint(19, 112))
-})
-sprites.onCreated(SpriteKind.Food, function (sprite) {
-    sprite.setImage(img`
-        . . . . . 3 3 b 3 3 d d 3 3 . . 
         . . . . 3 1 1 d 3 d 1 1 1 1 3 . 
         . . . 3 d 1 1 1 d 1 1 1 d 3 1 3 
         . . 3 d d 1 1 1 d d 1 1 1 3 3 3 
@@ -64,11 +72,146 @@ sprites.onCreated(SpriteKind.Food, function (sprite) {
         `)
     sprite.setPosition(randint(8, 152), randint(19, 112))
 })
+sprites.onCreated(SpriteKind.Food, function (sprite) {
+    sprite.setImage(img`
+        . . . . . . . . . . . 6 6 6 6 6 
+        . . . . . . . . . 6 6 7 7 7 7 8 
+        . . . . . . 8 8 8 7 7 8 8 6 8 8 
+        . . e e e e c 6 6 8 8 . 8 7 8 . 
+        . e 2 5 4 2 e c 8 . . . 6 7 8 . 
+        e 2 4 2 2 2 2 2 c . . . 6 7 8 . 
+        e 2 2 2 2 2 2 2 c . . . 8 6 8 . 
+        e 2 e e 2 2 2 2 e e e e c 6 8 . 
+        c 2 e e 2 2 2 2 e 2 5 4 2 c 8 . 
+        . c 2 e e e 2 e 2 4 2 2 2 2 c . 
+        . . c 2 2 2 e e 2 2 2 2 2 2 2 e 
+        . . . e c c e c 2 2 2 2 2 2 2 e 
+        . . . . . . . c 2 e e 2 2 e 2 c 
+        . . . . . . . c e e e e e e 2 c 
+        . . . . . . . . c e 2 2 2 2 c . 
+        . . . . . . . . . c c c c c . . 
+        `)
+    sprite.setPosition(randint(8, 152), randint(19, 112))
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food3, function (sprite, otherSprite) {
+    chicken.destroy()
+    info.changeScoreBy(1)
+})
+sprites.onCreated(SpriteKind.Food4, function (sprite) {
+    sprite.setImage(img`
+        . . . . c c c b b b b b . . . . 
+        . . c c b 4 4 4 4 4 4 b b b . . 
+        . c c 4 4 4 4 4 5 4 4 4 4 b c . 
+        . e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+        e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+        e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+        e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+        . e b 4 4 4 4 4 5 4 4 4 4 b e . 
+        8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+        8 7 2 e e e e e e e e e e 2 7 8 
+        e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+        e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+        e b e 8 8 c c 8 8 c c c 8 e b e 
+        e e b e c c e e e e e c e b e e 
+        . e e b b 4 4 4 4 4 4 4 4 e e . 
+        . . . c c c c c e e e e e . . . 
+        `)
+    sprite.setPosition(randint(8, 152), randint(19, 112))
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food5, function (sprite, otherSprite) {
+    Apple.destroy()
+    info.changeScoreBy(1)
+})
+sprites.onDestroyed(SpriteKind.Food3, function (sprite) {
+    pause(500)
+    chicken = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Food3)
+})
+sprites.onDestroyed(SpriteKind.Food4, function (sprite) {
+    pause(500)
+    Hamburger = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Food4)
+})
+sprites.onDestroyed(SpriteKind.Food5, function (sprite) {
+    pause(500)
+    Apple = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Food5)
+})
+sprites.onCreated(SpriteKind.Food3, function (sprite) {
+    sprite.setImage(img`
+        . . 2 2 b b b b b . . . . . . . 
+        . 2 b 4 4 4 4 4 4 b . . . . . . 
+        2 2 4 4 4 4 d d 4 4 b . . . . . 
+        2 b 4 4 4 4 4 4 d 4 b . . . . . 
+        2 b 4 4 4 4 4 4 4 d 4 b . . . . 
+        2 b 4 4 4 4 4 4 4 4 4 b . . . . 
+        2 b 4 4 4 4 4 4 4 4 4 e . . . . 
+        2 2 b 4 4 4 4 4 4 4 b e . . . . 
+        . 2 b b b 4 4 4 b b b e . . . . 
+        . . e b b b b b b b e e . . . . 
+        . . . e e b 4 4 b e e e b . . . 
+        . . . . . e e e e e e b d b b . 
+        . . . . . . . . . . . b 1 1 1 b 
+        . . . . . . . . . . . c 1 d d b 
+        . . . . . . . . . . . c 1 b c . 
+        . . . . . . . . . . . . c c . . 
+        `)
+    sprite.setPosition(randint(8, 152), randint(19, 112))
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
-    ice_cream.destroy()
+    cherry.destroy()
     info.changeScoreBy(1)
 })
 sprites.onDestroyed(SpriteKind.Food2, function (sprite) {
+    pause(500)
     ice_cream = sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
@@ -89,11 +232,14 @@ sprites.onDestroyed(SpriteKind.Food2, function (sprite) {
         `, SpriteKind.Food2)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food2, function (sprite, otherSprite) {
-    ice_cream_2.destroy()
+    ice_cream.destroy()
     info.changeScoreBy(1)
 })
-let ice_cream_2: Sprite = null
+let Apple: Sprite = null
+let Hamburger: Sprite = null
+let chicken: Sprite = null
 let ice_cream: Sprite = null
+let cherry: Sprite = null
 info.startCountdown(30)
 game.splash("The Eating Game")
 game.splash("Arrows to move")
@@ -117,7 +263,7 @@ let mySprite = sprites.create(img`
     . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-ice_cream = sprites.create(img`
+cherry = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -135,7 +281,7 @@ ice_cream = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Food)
-ice_cream_2 = sprites.create(img`
+ice_cream = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -153,6 +299,60 @@ ice_cream_2 = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Food2)
+chicken = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Food3)
+Hamburger = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Food4)
+Apple = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Food5)
 mySprite.setBounceOnWall(true)
 game.onUpdate(function () {
     mySprite.x += controller.dx(175)
